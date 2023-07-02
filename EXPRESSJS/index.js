bef56d57204f082b4f26248c09bafa1c72ca7466
Alper Koşay <83144508+Awoked@users.noreply.github.com>
2023-07-02 15:56:56 +0300

    update

--- EXPRESSJS/index.js
+++ EXPRESSJS/index.js
@@ -5,6 +5,8 @@ const app = express();
 const port = 5500;
 
 
+// bir klasörü veya dosyayı public hale getirir
+app.use('^/assets', express.static('assets'))
 
 
 app.get("/", (req, res) => {
@@ -16,10 +18,28 @@ app.get("/user", (req, res) => {
     // res.send('User')
 })
 
-app.get("/user:id", (req, res) => {
+app.get("/user/:id", (req, res) => {
     res.send(`User: ${req.params.id}`)
 })
 
+
+app.get('/test', (req, res) => {
+    res.redirect('/user');
+})
+
+app.get('/getjson', (req, res) => {
+    res.json([
+        {
+            id: 1,
+            name: "alper"
+        },
+        {
+            id: 2,
+            name: "alper2"
+        }
+    ])
+    res.json({ id: 1, name: "Alper" })
+})
 
 app.listen(port, () => {
     console.log(`proje http://localhost:${port} üzerinden çalışıyor`);
