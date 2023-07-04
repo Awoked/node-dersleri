a1d63f2540cf645fe68637f85193e29017e4cf46
Alper Koşay <alper.kossay@gmail.com>
2023-07-04 19:07:13 +0300

    update

--- API/index.js
+++ API/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
+const path = require('path');
 const app = express();
-const port = 5500;
+const port = 8080;
 
 const router = express.Router();
 
@@ -19,16 +20,53 @@ let myData = [
     }
 ]
 
-router.get('/users', (req, res) => {
-    res.status(200).json(myData)
+router.get('/', (req, res) => {
+    // res.json({ id: 1 });
+
+    // res.status(400).json({ id: 1 })
+
+    // res.sendFile(path.join(__dirname, '/index.txt'));
+
+    // res.cookie('myCookie', 'deneme', {
+    //     expires: new Date(Date.now() + 12000),
+    //     domain: 'localhost',
+    //     path: '/',
+    //     httpOnly: true
+    // })
+    // res.status(200).json({ message: "asd" })
+
+    // res.attachment(path.resolve("/index.txt"));
+    // res.sendFile(path.join(__dirname, '/index.txt'));
+    // res.download('/index.txt');
+
+    res.setHeader('test', 'testValue')
+
+    res.send('ok');
 })
 
+//Bütün kullanıcılar
+router.get('/users', (req, res) => {
+    console.log('url', req.url);
+    console.log('ctype', req.is('html'));
+    console.log('getType', req.get('Content-Type'));
+    console.log('hostname', req.hostname);
+    console.log('protocol', req.protocol);
+    console.log('ip', req.ip);
+    console.log('httpVersion', req.httpVersion);
+    console.log('path', req.path);
+    console.log('isSecure', req.secure);
+
+    res.status(200).json(myData);
+})
+
+//ID'ye göre kullanıcılar
 router.get('/users/:id', (req, res) => {
     const user = myData.find(data => data.id === req.params.id);
 
     res.status(200).json(user);
 })
 
+//Kullanıcı ekle
 router.post('/users', (req, res) => {
     const { name, surname } = req.body;
     const newUser = {
@@ -54,6 +92,7 @@ router.put('/users/:id', (req, res) => {
     res.status(200).json(myData[findIndex]);
 })
 
+//Silme işlemi
 router.delete('/users/:id', (req, res) => {
     const { id } = req.params;
     const newUserList = myData.filter(data => data.id !== id);
